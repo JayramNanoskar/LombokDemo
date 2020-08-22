b188aacfa784ae54c3ff31931df9ca36d79a6832
JayramNanoskar <jayram.n978@gmail.com>
2020-08-23 02:22:49 +0530

    first commit

---
+++ src/main/Top.java
@@ -0,0 +1,15 @@
+package main;
+
+import model.Employee;
+
+public class Top {
+
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		System.out.println("Lombok Demo");
+		e.setName("John");
+		System.out.println(e.getName());
+
+	}
+
+}
